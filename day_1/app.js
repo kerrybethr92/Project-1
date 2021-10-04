22b022e6c220bf9cfdfb36416a68b70d0d288cd7
Kerry Beth Richardson <kerrybethrichardson@Kerrys-MacBook-Pro-2.local>
2021-10-04 08:52:38 -0500

    serach returns results

--- day_1/app.js
+++ day_1/app.js
@@ -6,28 +6,29 @@ let searchWord = ''
 
 $(() => {
 
-    $('input[type="submit"]').on('click', (event) => {
+    $('input[type="submit"]').on('click', (event) => { // revisit w03d02 DOM INPUT to see if this form can be better
         event.preventDefault();
-        searchType = $(event.currentTarget).val()
+        searchType = $(event.currentTarget).val();
+        // console.log(searchType);
         if ($('input[type="text"]').val()) {
             searchWord = $('input[type="text"]').val();
         } else {
             alert("Please enter a name or type to search.")
         }
         // console.log(searchType + searchWord);
+        // $(event.target).trigger('reset')
+        $.ajax({
+            url:`https://pokeapi.co/api/v2/${searchType}/${searchWord}`
+        }).then(
+            (data) => {
+                console.log(data);
+                console.log(data.name);
+            },
+            () => {
+                console.log('bad request');
+            }
+        )
     })
 
-    // $.ajax({
-    //     url:`https://pokeapi.co/api/v2/${searchType}/${searchWord}`
-    // }).then(
-    //     (data) => {
-    //
-    //         console.log(data);
-    //         console.log(data.name);
-    //
-    //     },
-    //     () => {
-    //         console.log('bad request');
-    //     }
-    // )
+
 })
