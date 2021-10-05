c65abd15925b52c566a2f4599d9689fcd37f9d36
Kerry Beth Richardson <kerrybethrichardson@Kerrys-MacBook-Pro-2.local>
2021-10-04 19:31:07 -0500

    moving card from search to team

--- app.js
+++ app.js
@@ -1,31 +1,49 @@
-const makeACard = (data) => {
-    const cardProperties = {
-        name: data.name,
-        pic: data.sprites.front_default,
-        type: data.types[0].type.name,
-        abilities: getAbilities(data.abilities)
-    }
-    const $card = $('<div>').addClass('card')
-    $card.append($('<h3>').text(cardProperties.name))
-    $card.append($('<h4>').text(cardProperties.type))
-    $card.append($('<img>').attr('src', cardProperties.pic))
-    const $abilitiesUL = $('<ul>').text('ABILITIES')
-    $card.append($abilitiesUL)
-    for (let a = 0; a < cardProperties.abilities; a++) {
-        $abilitiesUL.append($('<li>').text(cardProperties.abilities[a]))
-    }
-    $('.search-results').append($card)
-}
-
-const getAbilities = (arr) => {
-    const abilitiesArr = []
-    for (let a = 0; a < arr.length; a++) {
-        abilitiesArr.push(arr[a].ability.name)
-    }
-    return abilitiesArr;
-}
-
 $(() => {
+    const makeACard = (data) => {
+        const cardProperties = {
+            name: data.name,
+            pic: data.sprites.front_default,
+            type: data.types[0].type.name,
+            abilities: getAbilities(data.abilities)
+        }
+        const $card = $('<div>').addClass('card')
+        $card.append($('<h3>').text(cardProperties.name))
+        $card.append($('<h4>').text(cardProperties.type))
+        $card.append($('<img>').attr('src', cardProperties.pic))
+        const $abilitiesUL = $('<ul>').text('ABILITIES')
+        $card.append($abilitiesUL)
+        for (let a = 0; a < cardProperties.abilities; a++) {
+            $abilitiesUL.append($('<li>').text(cardProperties.abilities[a]))
+        }
+        const $newButton = $('<button>').text('Add to My Team').addClass('addToTeam')
+        $card.append($newButton)
+        $('.search-results').append($card)
+
+
+        $newButton.on('click', (event) => {
+            const $newTeamMember = $('<div>')
+            $newTeamMember.append($(event.target).parent())
+            $('.myTeam').append($newTeamMember)
+            // $('.myTeam').append($(event.target).parent())
+        })
+    }
+
+    const makeList = (data) => {
+        const results = []
+        for (let i = 0; i < data.pokemon.length; i++) {
+            results.push(data.pokemon[i].pokemon.name);
+        }
+        return results;
+    }
+
+    const getAbilities = (arr) => {
+        const abilitiesArr = []
+        for (let a = 0; a < arr.length; a++) {
+            abilitiesArr.push(arr[a].ability.name)
+        }
+        return abilitiesArr;
+    }
+
     // store search criteria in variables
     $('input[type="submit"]').on('click', (event) => {
         event.preventDefault();
@@ -40,16 +58,28 @@ $(() => {
             }).then((data) => {
                 makeACard(data);
             })
-        }
-    })
+        } else if (searchType === 'type') {
+            $.ajax({
+                url: `https://pokeapi.co/api/v2/${searchType}/${searchTerm}`
+            }).then((data) => {
+                const resultsList = makeList(data)
 
-    $('.card').on('click', (event) => {
-        $('.myTeam').append(event.target)
+            for (let p = 0; p < resultsList.length; p++) {
+                let pokemonName = resultsList[p];
+                $.ajax({
+                    url: `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+                }).then((data) => {
+                    makeACard(data);
+                })
+                }
+            })
+        }
+
     })
 })
 
 
-    // convert data to 'card' to be appended to search results
+// convert data to 'card' to be appended to search results
 
-    // add button or event listener to each card, when clicked it moves to 'myTeam'
-    // make sure it adds only 1 div, and not a div for every result from search.
+// add button or event listener to each card, when clicked it moves to 'myTeam'
+// make sure it adds only 1 div, and not a div for every result from search.
